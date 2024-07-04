0c05b7200dfd21a1f1956c255ae4eb5ca1601b30
FelipeGS998 <felipe.gutierrez.salamanca@gmail.com>
2024-07-04 03:57:25 -0400

    Primer commit - Actualizacion 3 del proyecto

--- Panel/callbacks/callbacks_Recomendacion.py
+++ Panel/callbacks/callbacks_Recomendacion.py
@@ -1,11 +1,30 @@
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output,State
 from data_loader import load_data
 import plotly.express as px
 
 def register_callbacks_layout1(app):
-    @app.callback(Output('Grafico_reco', 'figure'),
-                    Input('Grafico_reco', 'id'))
-    def update_figure(input_value):
-        df = load_data()
-        return px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-            
+    @app.callback(
+    [Output('link-1', 'style'),
+     Output('link-2', 'style'),
+     Output('link-3', 'style')],
+    [Input('navegador', 'pathname')]
+    )
+
+    def update_link_styles(pathname):
+        # Estilos base para los enlaces
+        base_style = {'fontSize': '20px', 'padding': '10px', 'textDecoration': 'none', 'color': 'black', 'margin': '0 20px'}
+        selected_style = {'fontSize': '30px', 'padding': '10px', 'textDecoration': 'none', 'color': 'blue', 'margin': '0 20px'}
+        
+        link1_style = base_style
+        link2_style = base_style
+        link3_style = base_style
+
+        # Update the selected link style
+        if pathname == '/page-1':
+            link1_style = selected_style
+        elif pathname == '/page-2':
+            link2_style = selected_style
+        elif pathname == '/page-3':
+            link3_style = selected_style
+
+        return link1_style, link2_style, link3_style
--- Panel/layouts/Recomendacion.py
+++ Panel/layouts/Recomendacion.py
@@ -3,7 +3,7 @@ import plotly.express as px
 from data_loader import load_data
 import dash_daq as daq
 import dash_bootstrap_components as dbc
-from utils import widget_fecha
+#from utils import widget_fecha
 from datetime import datetime
 from datetime import date
 import plotly.graph_objects as go #Módulo para crear gráficos
@@ -33,9 +33,42 @@ import plotly.express as px #Módulo para crear gráficos
 marks = {i*60: f'{i:02}:00' for i in range(0, 25, 6)}
 marks[1440] = '24:00'  # Agregar marca para las 24:00
 
+muestras=['Calidad Chapa', 'Calidad Tablero', 'Meta']
+
+garfico_cantidad = go.Figure(data=[
+    go.Bar(name='Cumplido', x=muestras, y=[20, 14, 23], marker_color = "#06544B"),
+    go.Bar(name='Rechazo', x=muestras, y=[12, 18, 29], marker_color = '#D7263D')])
+
+garfico_cantidad.update_traces(marker_line_width=1.5, opacity=0.6)
+
+garfico_cantidad.update_layout(
+    xaxis_title='Categoria',
+    yaxis_title='Cantidad',
+    margin=dict(l=0, r=0, t=0, b=0),  # Elimina márgenes
+    paper_bgcolor='rgba(0,0,0,0)',  # Fondo del contenedor transparente
+    plot_bgcolor='rgba(0,0,0,0)',   # Fondo de la gráfica transparente
+    xaxis=dict(
+        showline=False,  # Elimina la línea del eje x
+        zeroline=False   # Elimina la línea cero
+    ),
+    yaxis=dict(
+        showline=False,  # Elimina la línea del eje y
+        zeroline=False   # Elimina la línea cero
+    ),
+    width=500,height=270,
+
+    font=dict(
+        family="Arimo ",   # Fuente del texto
+        size=10,          # Tamaño del texto
+        color="#DFD1A7"     # Color del texto
+    ),
+    barmode='group')
+
+
+
 def grafico_indicador_movile(id,label,style_edit):
     alpha = dbc.Row([
-            dbc.Col(daq.LEDDisplay(id = id,label=label,value=0 ,size=20,color="#000000", className = "CAJAS"),md = 1),
+            dbc.Col(daq.LEDDisplay(id = id,label=label,value=4 ,size=20,color="#000000", className = "CAJAS"),md = 1),
             dbc.Col(dcc.Graph(id = str('alert_' + id), config = {"displayModeBar": False},figure = indicador_targeta(10)),style = {'margin-top': -30,'textAlign': 'right','margin-left':-8,'line-left': 0,'position': 'relative','top': '44px','left': '38px'} , md = 1)
             ], style = style_edit)
     return alpha
@@ -49,8 +82,18 @@ def indicador_targeta(valor):
     return fig
 
 def create_layout(app):
+    marks = {i*60: f'{i:02}:00' for i in range(0, 25, 6)}
+    marks[1440] = '24:00'  # Agregar marca para las 24:00
     now = datetime.now()
     recomendacion =html.Div([
+        html.Div([
+                    dcc.Location(id='navegador',refresh=False),
+                    html.Div([
+                        dcc.Link('Simulacion', href='/Simulacion',id='link-2', className='link'),
+                        dcc.Link('Recomendacion', href='/Recomendacion',id='link-1', className='link'),
+                        dcc.Link('Refuerzo', href='/Refuerzo',id='link-3', className='link')
+                    ], id='link-container', className='link-container')
+                ]),
 
         dbc.Card(html.Div(
             ), className = "recomendacion_card_1"),
@@ -62,6 +105,8 @@ def create_layout(app):
             ), className = "recomendacion_card_4"),
         dbc.Card(html.Div(
             ), className = "recomendacion_card_5"),
+        dbc.Card(html.Div(
+            ), className = "recomendacion_card_6"),
         html.Div(className="Logo_UBB"),
         html.Div(className="Logo_Arauco"),
         html.Div(className="Logo_ANID"),
@@ -85,8 +130,24 @@ def create_layout(app):
         dcc.RangeSlider(min=0,max=1440, marks=marks, value=[300, 900],className='custom-range-slider'),
         html.H2('Fecha', className = "nombre_fecha"),
         html.H2('Hora', className = "nombre_hora"),
-        grafico_indicador_movile("led","Cantidad",{'margin-top' : -100,'margin-left': 540,'margin-right': 0,'width': 160,'height': 100,'textAlign': 'left', 'position': 'absolute'}),
+        grafico_indicador_movile("led","Cantidad",{'margin-top' : -100,'margin-left': 560,'margin-right': 0,'width': 160,'height': 100,'textAlign': 'left', 'position': 'absolute'}),
         html.H2('Calidad Chapa', className = "nombre_chapa"),
+        dbc.Badge("Cumplido", color="success",className="Aprobado_chapa"),
+        html.H2('Objetivo', className = "objetivo_chapa"),
+        dbc.Button("Revisar", color="secondary", className="Revisar_chapa"),
+        html.H2('Calidad Tablero', className = "nombre_tablero"),
+        grafico_indicador_movile("led","Cantidad",{'margin-top' : -100,'margin-left': 920,'margin-right': 0,'width': 160,'height': 100,'textAlign': 'left', 'position': 'absolute'}),
+        dbc.Badge("Rechazo", color="danger",className="Rechazo_tablero"),
+        html.H2('Objetivo', className = "objetivo_tablero"),
+        dbc.Button("Revisar", color="secondary", className="Revisar_tablero"),
+        html.H2('Meta', className = "nombre_meta"),
+        grafico_indicador_movile("led","Cantidad",{'margin-top' : -100,'margin-left': 1285,'margin-right': 0,'width': 160,'height': 100,'textAlign': 'left', 'position': 'absolute'}),
+        dbc.Badge("Cumplido", color="success",className="Aprobado_meta"),
+        html.H2('Objetivo', className = "objetivo_meta"),
+        dbc.Button("Revisar", color="secondary", className="Revisar_meta"),
+        dcc.Graph(config = {"displayModeBar": False},figure = garfico_cantidad,className = 'grafico_cantidad_kpi')
+
+
                             ],style = {'background': '#DFD1A7','height': '100vh','posicion': 'absolute'}
                             )
     return recomendacion
