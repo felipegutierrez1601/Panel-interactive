227dc9299ae02b4fbdbe2fe21756e6006ba62685
FelipeGS998 <felipe.gutierrez.salamanca@gmail.com>
2024-07-17 22:57:51 -0400

    Primer commit - edición 5 del proyecto

--- Panel/data_loader.py
+++ Panel/data_loader.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 def load_data():
-    # Datos de ejemplo
-    return pd.DataFrame({
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-    })
+    mi_conexion = pyodbc.connect(
+                Trusted_Connection='No',
+                Authentication='ActiveDirectoryPassword',
+                UID='Felipe',
+                PWD= 'Fondef',
+                Driver='{SQL Server}',
+                Server='146.83.131.135',
+                Database='Arauco')
+    data = pd.read_sql('SELECT * FROM KPIS', mi_conexion)
+    return data
--- Panel/layouts/Simulacion.py
+++ Panel/layouts/Simulacion.py
@@ -20,3 +20,24 @@ def create_layout(app):
         )
     ])
 """
+
+def create_layout(app):
+
+    simulacion = html.Div([
+
+        html.Div([
+                    dcc.Location(id='navegador',refresh=False),
+                    html.Div([
+                        dcc.Link(
+                            [
+                                html.Img(src='/assets/home_2.png'),  # Imagen como icono
+                            ],
+                            href='/Principal',className='icon-link'),
+                        html.Div(dcc.Link('Refuerzo', href='/Refuerzo',id='link-2'),className='link',style={'margin-left':269}),
+                        html.Div(dcc.Link('Simulación', href='Simulacion',id='link-1'),className ='link2'),
+                        html.Div(dcc.Link('Recomendación', href='/Recomendacion',id='link-3'),style= {'margin-left' : 420,'margin-top':'2px','font-family': 'Scene', 'padding': '2px' '5px','background-color': '#9AC4EE','border-radius': '15px','border': '2px solid #000000','width':'190px','font-size': '1px','height': '37px'}),
+                    ], id='link-container', className='link-container')
+                ]),
+                        ],style = {'background': '#FFFFFF','height': '100vh','posicion': 'absolute'})
+
+    return simulacion
